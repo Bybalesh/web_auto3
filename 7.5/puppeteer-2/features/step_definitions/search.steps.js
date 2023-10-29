6253da0dde715ccb64785d3413e28c55b198469c
Nikita Kruglov <bybalesh234@gmail.com>
2023-10-29 18:25:52 +0600

    Timeout's commit

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -1,12 +1,12 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
+const { Given1, When1, Then1, Given2, When2, Then2, Given3, When3, Then3, Before, After } = require("cucumber");
 const { getText, clickElement, ButtonIsEnabled } = require("../../lib/commands.js");
 
 Before(async function () {
 	this.browser = await puppeteer.launch({
-		headless: false, slowMo: 500, defaultViewport: null,
+		headless: false, slowMo: 30, defaultViewport: null,
 		args: ['--start-maximized'],
 	});
 	this.page = await this.browser.newPage();
@@ -20,44 +20,50 @@ After(async function () {
 	}
 });
 
-Given("user is on the booking page", async function () { //1 сценарий
+Given1("user is on the booking page", async function () { //1 сценарий
 });
-When("user chooses a ticket", async function () {
-	await clickElement(this.page, 'body > nav > a:nth-child(5)');
-	await clickElement(this.page, 'body > main > section:nth-child(1) > div.movie-seances__hall > ul > li > a');
-	await this.page.waitForSelector('body > main > section > div.buying-scheme > div.buying-scheme__wrapper');
-	await clickElement(this.page, 'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(7) > span:nth-child(5)');
+When1("user chooses a ticket", async function () {
+	await clickElement(this.page, 'nav > a:nth-child(5)');
+	//await this.page.waitForTimeout(500);
+	await clickElement(this.page, 'main > section:nth-child(1) > div.movie-seances__hall > ul > li > a');
+	await this.page.waitForSelector('main > section > div.buying-scheme > div.buying-scheme__wrapper');
+	await clickElement(this.page, '.buying-scheme__wrapper > div:nth-child(7) > span:nth-child(5)');
 });
-Then("the booking button for single ticket should be enabled", async function () {
-	const isButtonActive = await buttonIsEnabled(this.page, 'body > main > section > button', 'disabled');
+Then1("the booking button for single ticket should be enabled", async function () {
+	//await this.page.waitForTimeout(1000);
+	const isButtonActive = await buttonIsEnabled(this.page, 'main > section > button', 'disabled');
 	expect(isButtonActive).to.be.false;
 });
 
 
-Given("user is on the booking page", async function () {  //2 сценарий
+Given2("user is on the booking page", async function () {  //2 сценарий
 });
-When("user chooses multiple tickets", async function () {
-	await clickElement(this.page, 'body > nav > a:nth-child(3)');
-	await clickElement(this.page, 'body > main > section:nth-child(2) > div.movie-seances__hall > ul > li > a');
-	await this.page.waitForSelector('body > main > section > div.buying-scheme > div.buying-scheme__wrapper');
-	await clickElement(this.page, 'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(8) > span:nth-child(18)');
-	await clickElement(this.page, 'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(5)');
+When2("user chooses multiple tickets", async function () {
+	await clickElement(this.page, 'nav > a:nth-child(3)');
+	//await this.page.waitForTimeout(5000);
+	await clickElement(this.page, 'main > section:nth-child(2) > div.movie-seances__hall > ul > li > a');
+	await this.page.waitForSelector('main > section > div.buying-scheme > div.buying-scheme__wrapper');
+	await clickElement(this.page, '.buying-scheme__wrapper > div:nth-child(8) > span:nth-child(18)');
+	await clickElement(this.page, '.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(5)');
 });
-Then("the booking button should be enabled", async function () {
-	const isButtonActive = await buttonIsEnabled(this.page, 'body > main > section > button', 'disabled');
+Then2("the booking button should be enabled", async function () {
+	//await this.page.waitForTimeout(1000);
+	const isButtonActive = await buttonIsEnabled(this.page, 'main > section > button', 'disabled');
 	expect(isButtonActive).to.be.false;
 });
 
 
-Given("user is on the booking page", async function () {  //3 сценарий
+Given3("user is on the booking page", async function () {  //3 сценарий
 });
-When("user chooses a booked ticket", async function () {
-	await clickElement(this.page, 'body > nav > a:nth-child(3)');
-	await clickElement(this.page, 'body > main > section:nth-child(2) > div.movie-seances__hall > ul > li > a');
-	await page.waitForSelector('body > main > section > div.buying-scheme > div.buying-scheme__wrapper');
-	await clickElement(this.page, 'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(10) > span.buying-scheme__chair.buying-scheme__chair_standart.buying-scheme__chair_taken');
+When3("user chooses a booked ticket", async function () {
+	await clickElement(this.page, 'nav > a:nth-child(2)');
+	//await this.page.waitForTimeout(1000);
+	await clickElement(this.page, 'main > section:nth-child(2) > div.movie-seances__hall > ul > li > a');
+	await page.waitForSelector('main > section > div.buying-scheme > div.buying-scheme__wrapper');
+	await clickElement(this.page, '.buying-scheme__wrapper > div:nth-child(4) > span:nth-child(10)');
 });
-Then("the booking button should be disabled", async function () {
-	const isButtonActive = await buttonIsEnabled(this.page, 'body > main > section > button', 'disabled');
+Then3("the booking button should be disabled", async function () {
+	//await this.page.waitForTimeout(1000);
+	const isButtonActive = await buttonIsEnabled(this.page, 'main > section > button', 'disabled');
 	expect(isButtonActive).to.be.true;
 });
--- 7.5/puppeteer-2/net.test.js
+++ 7.5/puppeteer-2/net.test.js
@@ -16,29 +16,29 @@ afterEach(() => {
 describe("ticket reservations tests", () => {
 
 	test("The first test", async () => { //1 позитивный тест
-		await clickElement(page, 'body > nav > a:nth-child(5)');
-		await clickElement(page, 'body > main > section:nth-child(1) > div.movie-seances__hall > ul > li > a');
-		await page.waitForSelector('body > main > section > div.buying-scheme > div.buying-scheme__wrapper');
+		await clickElement(page, 'nav > a:nth-child(5)');
+		await clickElement(page, 'main > section:nth-child(1) > div.movie-seances__hall > ul > li > a');
+		await page.waitForSelector('main > section > div.buying-scheme > div.buying-scheme__wrapper');
 		await clickElement(page, '.buying-scheme__wrapper > div:nth-child(7) > span:nth-child(5)');
-		const isButtonActive = await buttonIsEnabled(page, 'body > main > section > button', 'disabled');
+		const isButtonActive = await buttonIsEnabled(page, 'main > section > button', 'disabled');
 		expect(isButtonActive).to.be.false;
 	});
 
 	test("Booking multiple tickets", async () => { //2 позитивный тест
-		await clickElement(page, 'body > nav > a:nth-child(3)');
-		await clickElement(page, 'body > main > section:nth-child(2) > div.movie-seances__hall > ul > li > a');
-		await page.waitForSelector('body > main > section > div.buying-scheme > div.buying-scheme__wrapper');
+		await clickElement(page, 'nav > a:nth-child(3)');
+		await clickElement(page, 'main > section:nth-child(2) > div.movie-seances__hall > ul > li > a');
+		await page.waitForSelector('main > section > div.buying-scheme > div.buying-scheme__wrapper');
 		await clickElement(page, '.buying-scheme__wrapper > div:nth-child(8) > span:nth-child(18)');
 		await clickElement(page, '.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(5)');
-		const isButtonActive = await buttonIsEnabled(page, 'body > main > section > button', 'disabled');
+		const isButtonActive = await buttonIsEnabled(page, 'main > section > button', 'disabled');
 		expect(isButtonActive).to.be.false;
 	});
 	test("reserved place", async () => { //негативный тест
-		await clickElement(page, 'body > nav > a:nth-child(2)');
-		await clickElement(page, 'body > main > section:nth-child(1) > div.movie-seances__hall > ul > li > a');
-		await page.waitForSelector('body > main > section > div.buying-scheme > div.buying-scheme__wrapper');
-		await clickElement(page, '.buying-scheme__chair.buying-scheme__chair_standart.buying-scheme__chair_taken');
-		const isButtonActive = await buttonIsEnabled(page, 'body > main > section > button', 'disabled');
+		await clickElement(page, 'nav > a:nth-child(2)');
+		await clickElement(page, 'main > section:nth-child(2) > div.movie-seances__hall > ul > li > a');
+		await page.waitForSelector('main > section > div.buying-scheme > div.buying-scheme__wrapper');
+		await clickElement(page, '.buying-scheme__wrapper > div:nth-child(4) > span:nth-child(10)');
+		const isButtonActive = await buttonIsEnabled(page, 'main > section > button', 'disabled');
 		expect(isButtonActive).to.be.true;
 	});
 });
